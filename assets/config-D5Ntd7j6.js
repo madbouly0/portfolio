b4e9325db9cc6e1c44a21bb75e8dd37ee1f83bd0
madbouly0 <madbouly0@users.noreply.github.com>
2026-03-06 17:15:11 +0000

    deploy: 56d3cca196634745df2b9e041d7d80ca60183666

---
+++ assets/config-D5Ntd7j6.js
@@ -0,0 +1,5 @@
+const n="Mahmoud Madbouly",o="data",t=["Engineer","Scientist","Analyst","Integration Engineer","Enthusiast"],i="",s="https://github.com/MahmoudMadbouly/portfolio",c=`__  __  __
+\\ \\ \\ \\ \\∕
+ \\ \\∕\\ \\
+  \\∕  \\∕
+`,e={name:n,role:o,disciplines:t,url:i,repo:s,ascii:c};export{e as c};
---
+++ assets/index-3Ta26qMp.js
@@ -0,0 +1,1 @@
+import{R as t}from"./jsx-runtime-BfF-YriY.js";const o={},s=t.createContext(o);function u(e){const n=t.useContext(s);return t.useMemo(function(){return{...n,...e}},[n,e])}export{u};
